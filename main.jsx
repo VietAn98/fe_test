2a81e35ed96422c2153b366632b0f04e49932507
VietAn98 <44429962+VietAn98@users.noreply.github.com>
2022-01-05 00:50:25 +0700

    Setup project structure, common tools

--- main.jsx
+++ main.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
